d5e5b4a8020b59fa305c5e7c5022ec5ca322ff37
Jovaun Robinson <jovaunrobinson@yahoo.com>
2016-11-08 00:57:01 +0000

    exercise 4

--- request.php
+++ request.php
@@ -14,4 +14,18 @@ $definition = [
 print "<h3>" . strtoupper($query) . "</h3>";
 
 print "<p>" . $definition[$query] . "</p>";
+/*
+$xmldata =
+'<?xml version="1.0" encoding="UTF-8"?><entries>
+    <definition name="definition" author="Jovaun">A statement of the exact meaning of a word, especially in a dictionary.</definition>
+    <definition name="bar" author="Deniro">A place that sells alcholic beverages</definition>
+    <definition name="ajax" author="Dalhouse">Technique which involves the use of javascript and xml (or JSON)</definition>
+    <definition name="html" author="Stephanie">The standard markup language for creating web pages and web applications.</definition>
+    <definition name="css" author="Monique">A style sheet language used for describing the presentation of a document written in a markup language</definition>
+    <definition name="javascript" author="Macdonald">A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</definition>
+    <definition name="php" author="Hudson">A server-side scripting language, and a powerful tool for making dynamic and interactive websites</definition>
+</entries>';
+header("Content-Type:text/xml");
+$xmld=new SimpleXMLElement($xmldata);
+echo $xmld->asXML();*/
 
